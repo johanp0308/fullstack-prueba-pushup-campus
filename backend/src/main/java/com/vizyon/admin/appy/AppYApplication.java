600da9720d25bde36e4439826adcac0a6363502a
johanp0308 <sebastian-johan1@hotmail.com>
2024-04-25 09:05:39 -0500

    feat: :tada: Init Commit

---
+++ backend/src/main/java/com/vizyon/admin/appy/AppYApplication.java
@@ -0,0 +1,13 @@
+package com.vizyon.admin.appy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppYApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppYApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vizyon/admin/appy/AppYApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizyon.admin.appy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppYApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
